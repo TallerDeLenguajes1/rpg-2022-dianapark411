6733bd75a66619b0d56d060f1a29ed6f75e5667e
Diana Park <dianapark411@gmail.com>
2022-06-14 16:11:48 -0300

    Avances 2

--- MyApp/Personajes.cs
+++ MyApp/Personajes.cs
@@ -1,3 +1,12 @@
+public class personaje{
+    public datos dat;
+    public caracteristicas carac;
+
+    public personaje(datos _dat, caracteristicas _carac){
+        dat = _dat; 
+        carac = _carac;
+    }
+}
 
 public enum Tipo{ 
     Brujo, 
@@ -5,14 +14,15 @@ public enum Tipo{
     Princesa, 
     Elfo,
     Dragon,
+    Demonio,
 }
-
 public enum Nombre{
     GeraltDeRivia,
     YenneferDeVenderberg,
     CirillaDeCintra,
     FilavandrelAénFidháil,
     Villentretenmerth, 
+    VolethMeir,
 }
 public enum Apodo{
     Geralt,
@@ -20,16 +30,16 @@ public enum Apodo{
     Ciri,
     Filavandrel,
     Borch, 
+    BabaYaga, 
 }
-public class personaje{
-    public class datos{
-        public Tipo tipo;
-        public Nombre Nombre;
-        public Apodo Apodo;
-        public DateTime FechaNacimiento {get;set;}
-        public int Edad {get;set;}
-        public int Salud {get;set;}
-    }
+
+public class datos{
+    public Tipo Tipo;
+    public Nombre Nombre;
+    public Apodo Apodo;
+    public DateTime FechaNacimiento {get;set;}
+    public int Edad {get;set;}
+    public int Salud {get;set;}
 
     DateTime fechaActual = DateTime.Today;
     public int edad = 0;
@@ -40,12 +50,74 @@ public class personaje{
         }
         return edad;
     }
-    public class caracteristicas{
-        public int Velocidad {get;set;}
-        public int Destreza {get;set;}
-        public int Fuerza {get;set;}
-        public int Nivel {get;set;}
-        public int Armadura {get;set;}
+    public DateTime RandomDay() {
+        DateTime start = new DateTime(1722, 1, 1); 
+        Random gen = new Random(); 
+        int range = (DateTime.Today - start).Days; 
+        return start.AddDays(gen.Next(range)); 
     }
 
+    public datos(int tipo){
+        // Random random = new Random();
+        //int tipo = random.Next(1,5);
+        
+        switch (tipo){
+            case 1:
+                Tipo = Tipo.Brujo;
+                Nombre = Nombre.GeraltDeRivia;
+                Apodo = Apodo.Geralt;
+                break;
+            case 2:
+                Tipo = Tipo.Hechicera;
+                Nombre = Nombre.YenneferDeVenderberg;
+                Apodo = Apodo.Yennefer;
+                break;
+            case 3:
+                Tipo = Tipo.Princesa;
+                Nombre = Nombre.CirillaDeCintra;
+                Apodo = Apodo.Ciri;
+                break;
+            case 4:
+                Tipo = Tipo.Elfo;
+                Nombre = Nombre.FilavandrelAénFidháil;
+                Apodo = Apodo.Filavandrel;
+                break;
+            case 5:
+                Tipo = Tipo.Dragon;
+                Nombre = Nombre.Villentretenmerth;
+                Apodo = Apodo.Borch;
+                break;
+            case 6:
+                Tipo = Tipo.Demonio;
+                Nombre = Nombre.VolethMeir;
+                Apodo = Apodo.BabaYaga;
+                break;
+            default:
+                break;
+        }
+
+        FechaNacimiento = RandomDay();
+
+        Edad = calcularEdad(FechaNacimiento);
+
+        Salud = 1000;
+    }
+}
+
+public class caracteristicas{
+    public int Velocidad {get;set;}
+    public int Destreza {get;set;}
+    public int Fuerza {get;set;}
+    public int Nivel {get;set;}
+    public int Armadura {get;set;}
+
+    public caracteristicas(){
+        Random random = new Random();
+
+        Velocidad = random.Next(1,10);
+        Destreza = random.Next(1,5);
+        Fuerza = random.Next(1,10);
+        Armadura = random.Next(1,10);
+        Nivel = random.Next(1,10);
+    }
 }
--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -1,95 +1,45 @@
 ﻿// See https://aka.ms/new-console-template for more information
+List<personaje> PersonajesEnJuego = new List<personaje>();
 
-personaje personaje = new personaje();
-personaje.datos datos = cargarDatos();
-personaje.caracteristicas caracteristicas = cargarCaracteristicas();
+//QUIERO QUE SE CARGUEN EN LA LISTA TODOS LOS PERSONAJES
+int cantPersonajes = 6;
 
-mostrarDatos(datos);
-mostrarCaracteristicas(caracteristicas);
+for (int i = 0; i < cantPersonajes; i++){
 
-personaje.datos cargarDatos(){
-
-    personaje.datos datos = new personaje.datos();
-    
-    Random random = new Random();
-    int tipo = random.Next(1,5);
-
-    switch (tipo){
-        case 1:
-            datos.tipo = Tipo.Brujo;
-            datos.Nombre = Nombre.GeraltDeRivia;
-            datos.Apodo = Apodo.Geralt;
-            break;
-        case 2:
-            datos.tipo = Tipo.Hechicera;
-            datos.Nombre = Nombre.YenneferDeVenderberg;
-            datos.Apodo = Apodo.Yennefer;
-            break;
-        case 3:
-            datos.tipo = Tipo.Princesa;
-            datos.Nombre = Nombre.CirillaDeCintra;
-            datos.Apodo = Apodo.Ciri;
-            break;
-        case 4:
-            datos.tipo = Tipo.Elfo;
-            datos.Nombre = Nombre.FilavandrelAénFidháil;
-            datos.Apodo = Apodo.Filavandrel;
-            break;
-        case 5:
-            datos.tipo = Tipo.Dragon;
-            datos.Nombre = Nombre.Villentretenmerth;
-            datos.Apodo = Apodo.Borch;
-            break;
-        default:
-            break;
-    }
-
-    datos.FechaNacimiento = RandomDay();
-
-    datos.Edad = personaje.calcularEdad(datos.FechaNacimiento);
-
-    datos.Salud = 100;
-
-    return datos;
+    datos datos = new datos(i+1);
+    caracteristicas caracteristicas = new caracteristicas();
+    personaje player = new personaje(datos, caracteristicas);
+    PersonajesEnJuego.Insert(i, player);
 }
 
-DateTime RandomDay() {
-    DateTime start = new DateTime(1722, 1, 1); 
-    Random gen = new Random(); 
-    int range = (DateTime.Today - start).Days; 
-    return start.AddDays(gen.Next(range)); 
+Console.WriteLine("\n------PERSONAJES DISPONIBLES------");
+for (int i = 0; i < cantPersonajes; i++){
+    mostrarUnPersonaje(PersonajesEnJuego[i], i);
 }
 
-personaje.caracteristicas cargarCaracteristicas(){
-    
-    personaje.caracteristicas caracteristicas = new personaje.caracteristicas();
+Console.WriteLine("\nElija dos personajes para que peleen separados por un guion (ejemplo 2-4): ");
+string eleccion = Console.ReadLine();
+//CASTEARLO DESPUES
+string[] eleccionArray = eleccion.Split('-');
+Console.WriteLine(String.Join(", ", eleccionArray));
 
-    Random random = new Random();
+//char[] delimiterChars = { ' ', ',', '.', ':', '-', '/'};
+//List<int> eleccionLista = eleccion?.Split(',').Select(Int32.Parse).ToList();
 
-    caracteristicas.Velocidad = random.Next(1,10);
-    caracteristicas.Destreza = random.Next(1,5);
-    caracteristicas.Fuerza = random.Next(1,10);
-    caracteristicas.Armadura = random.Next(1,10);
-    caracteristicas.Nivel = random.Next(1,10);
+void mostrarUnPersonaje(personaje _personaje, int i){
+    Console.WriteLine($"\n----INFORMACION DEL PERSONAJE [{i+1}]----");
+    //Console.WriteLine("\n--------DATOS DEL PERSONAJE--------");
+    Console.WriteLine($"Tipo: {_personaje.dat.Tipo}");
+    Console.WriteLine($"Nombre: {_personaje.dat.Nombre}");
+    Console.WriteLine($"Apodo: {_personaje.dat.Apodo}");
+    Console.WriteLine($"Fecha de nacimiento: {_personaje.dat.FechaNacimiento.ToShortDateString()}");
+    Console.WriteLine($"Edad: {_personaje.dat.Edad}");
+    Console.WriteLine($"Salud: {_personaje.dat.Salud}");
 
-    return caracteristicas;
-}
-
-void mostrarDatos(personaje.datos datos){
-    Console.WriteLine("\n-------DATOS DEL PERSONAJE-------");
-    Console.WriteLine($"Tipo: {datos.tipo}");
-    Console.WriteLine($"Nombre: {datos.Nombre}");
-    Console.WriteLine($"Apodo: {datos.Apodo}");
-    Console.WriteLine($"Fecha de nacimiento: {datos.FechaNacimiento.ToShortDateString()}");
-    Console.WriteLine($"Edad: {datos.Edad}");
-    Console.WriteLine($"Salud: {datos.Salud}");
-}
-
-void mostrarCaracteristicas(personaje.caracteristicas caracteristicas){
-    Console.WriteLine("\n-----CARACTERISTICAS DEL PERSONAJE-----");
-    Console.WriteLine($"Velocidad: {caracteristicas.Velocidad}");
-    Console.WriteLine($"Destreza: {caracteristicas.Destreza}");
-    Console.WriteLine($"Fuerza: {caracteristicas.Fuerza}");
-    Console.WriteLine($"Nivel: {caracteristicas.Nivel}");
-    Console.WriteLine($"Armadura: {caracteristicas.Armadura}");
+    //Console.WriteLine("\n----CARACTERISTICAS DEL PERSONAJE----");
+    Console.WriteLine($"Velocidad: {_personaje.carac.Velocidad}");
+    Console.WriteLine($"Destreza: {_personaje.carac.Destreza}");
+    Console.WriteLine($"Fuerza: {_personaje.carac.Fuerza}");
+    Console.WriteLine($"Nivel: {_personaje.carac.Nivel}");
+    Console.WriteLine($"Armadura: {_personaje.carac.Armadura}");
 }
